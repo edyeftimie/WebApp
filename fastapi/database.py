41e64a0dbdbe9b0b361bd74a3dd88d285abe6321
Eftimie Eduard <eduh@DESKTOP-LQ1SO6G.localdomain>
2024-06-04 04:54:44 +0300

    another migratios to aws database

--- fastapi/database.py
+++ fastapi/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "mysql+pymysql://root:admin@localhost:3306/sqldb"
+from key import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 
